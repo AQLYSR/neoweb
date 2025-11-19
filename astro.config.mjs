3e02cf9da66f2225fccad3f602b1ab119ca7b5e9
AQLYSR <aqilyusri@proton.me>
2025-11-19 18:40:41 +0800

    fix hopefully

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://AQLYSR.github.io',
-    base: '/neoweb',
-    prefetch: true,
+  site: "https://AQLYSR.github.io",
+  base: process.env.NODE_ENV === "production" ? "/neoweb" : "/",
+  prefetch: true,
 });
