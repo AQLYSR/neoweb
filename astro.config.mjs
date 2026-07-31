92822ce4c8b2010fd6ffb629972107438951df36
AQLYSR <aqilyusri@proton.me>
2026-07-31 10:14:29 +0800

    update nodejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   base: "/",
   site: "https://aqilyusri.com",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
